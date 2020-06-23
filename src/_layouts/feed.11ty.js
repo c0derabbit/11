1753a2fe07f44dd4323171b9904a11ad628e1980
eszter <ekov@protonmail.ch>
2020-06-23 20:44:21 +0100

    add post excerpt

--- src/_layouts/feed.11ty.js
+++ src/_layouts/feed.11ty.js
@@ -13,6 +13,20 @@ const locales = {
 
 const i18n = lang => locales[lang] || locales['en']
 
+const excerpt = content => {
+  const images = /<img[^>]+>/g
+  const videos = /<video[^>]+>[^<]+<\/video>/g
+  const first20words = content
+    .replace(images, '')
+    .replace(videos, '')
+    .split(' ').slice(0, 20).join(' ')
+
+  if (/[\w,]$/.test(first20words))
+    return first20words + '…'
+
+  return first20words
+}
+
 exports.data = {
   layout: 'base.11ty.js',
 }
@@ -25,13 +39,14 @@ exports.render = ({ lang, collections }) => {
 
   return `
     <ul>
-      ${postsReversed.map(({ data: { title, page: { date, url }, content }}) => `
+      ${postsReversed.map(({ data: { title, page: { date, url }, content, description }}) => `
         <li class="mb-8">
           <a href="${url}">
             <time class="italic text-sm text-gray-600">
               ${dayjs(date).format(t('dateFormat'))}
             </time>
             <h2 class="my-1 text-xl font-bold">${title}</h2>
+            ${description || excerpt(content)}
           </a>
         </li>
       `).join('')}
