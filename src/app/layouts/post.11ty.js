8e30c38f0b246f8fda04c9201e0f4a6a4ec34a7c
eszter <eszter@fidel.uk>
2020-07-03 11:09:48 +0100

    improve lazyload & post typography

--- src/app/layouts/post.11ty.js
+++ src/app/layouts/post.11ty.js
@@ -8,7 +8,10 @@ exports.data = {
 exports.render = ({ title, page, content, lang }) => {
   const t = i18n(lang)
 
-  const lazy = content => content.replace(/(?<=<img[\s\S]+)(<img src)/g, '<img data-src')
+  const lazy = content => content.replace(
+    /(?<=<img[\s\S]+)(<img src)/g,
+    `<img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 3 2'%3E%3C/svg%3E" data-src`
+  )
 
   return `
     <a href="/${lang}">${t('back')}</a>
@@ -23,35 +26,32 @@ exports.render = ({ title, page, content, lang }) => {
 
         if ('IntersectionObserver' in window) {
           var observer = new IntersectionObserver(lazyload, {})
-          images[0].classList.add('fade-in')
+          images[0].style.opacity = 1
           for (var i = 1; i < images.length; i++) {
-            var image = images[i]
-            image.style.opacity = 0
-            image.style.animation = 'none'
-            observer.observe(image)
+            observer.observe(images[i])
           }
         } else {
           for (var i = 1; i < images.length; i++) {
             var image = images[i]
             image.setAttribute('src', image.dataset.src)
+            image.style.opacity = 1
           }
         }
 
         function lazyload(entries, observer) {
           for (var i = 0; i < entries.length; i++) {
             var entry = entries[i]
+
             if (entry.isIntersecting) {
               var target = entry.target
               var src = target.dataset.src
 
               var loader = new Image()
               loader.onload = function() {
+                target.setAttribute('src', src)
                 target.style.opacity = 1
               }
               loader.src = src
-              target.setAttribute('src', src)
-
-              observer.unobserve(target)
             }
           }
         }
