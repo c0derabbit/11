f44ee2e0578c09be34760e50d4f4349722963dd2
eszter <ekov@protonmail.ch>
2020-07-15 22:29:44 +0100

    fix syntax

--- src/app/layouts/post.11ty.js
+++ src/app/layouts/post.11ty.js
@@ -17,7 +17,6 @@ exports.render = ({ title, page, content, lang }) => {
        <img
         src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 3 2'%3E%3C/svg%3E"
         data-src=${url} `
-    }
   )
 
   return `
